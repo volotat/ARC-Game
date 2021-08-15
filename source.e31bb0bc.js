002c029bda95dad5969adfa215786bab645315d9
Alex K <bangbangfmgames@gmail.com>
2021-08-15 19:53:28 +0300

    fixed slash issue

--- source.e31bb0bc.js
+++ source.e31bb0bc.js
@@ -1982,7 +1982,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "3653" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6913" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -2159,4 +2159,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["C:/Users/admin/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/source.e31bb0bc.js.map
+//# sourceMappingURL=source.e31bb0bc.js.map
