0271d053ecd5a2a6f476c7af80b17e9a0d00a4b7
Alex K <bangbangfmgames@gmail.com>
2021-08-15 19:48:18 +0300

    update

--- source.e31bb0bc.js
+++ source.e31bb0bc.js
@@ -1982,7 +1982,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "8144" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "13440" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
